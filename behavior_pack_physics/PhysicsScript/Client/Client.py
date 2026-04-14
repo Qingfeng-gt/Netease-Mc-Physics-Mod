18b378224fcf9311a30a837fc0c5f2216a543e77
FlyQingfeng <2758534648@qq.com>
2026-04-14 16:05:30 +0800

    优化核心

--- behavior_pack_physics/PhysicsScript/Client/Client.py
+++ behavior_pack_physics/PhysicsScript/Client/Client.py
@@ -1,7 +1,13 @@
 from ..Core.Client.EventHandler.ClientEventHandler import  AutoRegisterClient
+from ..Core.Client.Lib.ClientLib import QfClientApi
 from ..Core.Client.ModClientBase import ModClientBase
+
 
 @AutoRegisterClient("physics_client")
 class Client(ModClientBase):
     def __init__(self, namespace, systemName):
         super(Client, self).__init__(namespace, systemName)
+
+    def OnUiInitFinished(self, args):
+        super(Client, self).OnUiInitFinished(args)
+
--- behavior_pack_physics/PhysicsScript/Server/Server.py
+++ behavior_pack_physics/PhysicsScript/Server/Server.py
@@ -1,3 +1,4 @@
+from ..Core.Common.Lib.Logger import Logger
 from ..Core.Server.EventHandler.ServerEventHandler import AutoRegisterServer
 from ..Core.Server.ModServerBase import ModServerBase
 
@@ -6,4 +7,8 @@ class Server(ModServerBase):
     def __init__(self, namespace, systemName):
         super(Server, self).__init__(namespace, systemName)
 
-    
+    def test(self,playerId,a,xxx):
+        Logger.debug("{}->{}:{}".format(playerId,a,xxx))
+
+
+
